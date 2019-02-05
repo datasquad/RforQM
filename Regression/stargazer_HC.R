983008474b9c593fae65dbd7d91498dde66c5248
Ralf Becker <ralffbecker@gmail.com>
2019-02-05 22:28:47 +0000

    First completed draft of Regression Intro 1

--- Regression/stargazer_HC.R
+++ Regression/stargazer_HC.R
@@ -1,5 +1,5 @@
-stargazer_HC <- function(mod) {
+stargazer_HC <- function(mod, type_in = "text") {
   cov1 <- vcovHC(mod, type = "HC1")
   robust_se <- sqrt(diag(cov1))
-  stargazer(mod,type="text",se=list(NULL, robust_se))
+  stargazer(mod,type=type_in,se=list(NULL, robust_se))
 }
