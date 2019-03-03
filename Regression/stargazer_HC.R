e87639186c6b7eabc1fc7f274b8dca0ae3e39f50
Ralf Becker <ralffbecker@gmail.com>
2019-03-03 23:23:09 +0000

    Work on Panel_Lecture_6

--- Regression/stargazer_HC.R
+++ Regression/stargazer_HC.R
@@ -1,7 +1,9 @@
-stargazer_HC <- function(..., type_out = "text", type_HC = "HC1") {
+stargazer_HC <- function(..., type_out = "text", type_HC = "HC1",omit.stat = NULL) {
   # the first inputs are n_mod models which are to be displayed
   # type_out carries through to the stargazer function, default: "text", other; "latex", "html"
   # type_HC carries through tothe vcovHC function, default: "HC1" (or Stata equivalent)
+  # omit_stat (default = NULL) will be handed through to stargazer, specify which stats you 
+  #      don't want to see. see ?stargazer help for more help
   mod_all <- list(...)  # model collection should be a list of models
   n_mod <- length(mod_all)
   rep_se <- list(NULL)  # use this to replace se
@@ -10,7 +12,11 @@ stargazer_HC <- function(..., type_out = "text", type_HC = "HC1") {
   if (length(mod_all) == 1) {
     cov1 <- vcovHC(mod_all[[1]], type = type_HC) # calculate next rob se
     robust_se <- sqrt(diag(cov1))
-    stargazer(mod_all[[1]],type=type_out,se=list(NULL,robust_se),notes="Robust standard errors in parenthesis")
+    stargazer(mod_all[[1]],
+              type=type_out,
+              se=list(NULL,robust_se),
+              omit.stat = omit.stat,
+              notes="Robust standard errors in parenthesis")
   } else {
     for (mod_i in mod_all){
       covi <- vcovHC(mod_i, type = type_HC) # calculate next rob se
@@ -19,6 +25,10 @@ stargazer_HC <- function(..., type_out = "text", type_HC = "HC1") {
       i <- i + 1
     }
     
-    stargazer(mod_all, type =type_out, se=rep_se, notes="Robust standard errors in parenthesis")
+    stargazer(mod_all, 
+              type =type_out, 
+              se=rep_se, 
+              omit.stat = omit.stat,
+              notes="Robust standard errors in parenthesis")
   }
 }
