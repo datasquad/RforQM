1856c74402c10030df7b2af7d1c9a8e57a48af19
Ralf Becker <ralffbecker@gmail.com>
2022-02-15 12:02:33 +0100

    Minor changes to lecture 2

---
+++ Data_Introduction/Comp0_working.R
@@ -0,0 +1,18 @@
+# This is the script to CompLab0
+
+setwd("C:/Rcode/RforQM/Data_Introduction")
+
+library(tidyverse)
+library(readxl) # to import Excel data
+library(ggplot2) # to produce nice graphiscs
+library(stargazer)
+
+CKdata <- read_excel("CK_public.xlsx", na = ".")
+
+str(CKdata)
+
+CKdata$STATEf <- as.factor(CKdata$STATE) # translates a variable into a factor variable
+levels(CKdata$STATEf) <- c("Pennsylvania","New Jersey") # changes the names of the categories
+
+CKdata$CHAINf <- as.factor(CKdata$CHAIN)
+levels(CKdata$CHAINf) <- c("BK","KFC", "Roys", "Wendy's")
